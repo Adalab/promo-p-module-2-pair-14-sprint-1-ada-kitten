8cef0c347bab0a6950741c4143875f6b169fad7c
MiriamPoveda <mpoveda94@gmail.com>
2022-03-21 11:16:17 +0100

    Add list variables + interpolation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,70 @@
 'use strict'; 
 
+// Modificar clase CSS 
+
 const newForm = document.querySelector('.js-new-form');
 newForm.classList.remove('collapsed');
+
+// Variables e interpolaciones (aplicadas en la parte inferior)
+
+const kittenOneImage = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
+const kittenOneName = "Anastacio";
+const kittenOneDesc = "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenOneRace = "British Shorthair";
+
+const kittenTwoImage = "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
+const kittenTwoName = "Fiona";
+const kittenTwoDesc = "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenTwoRace = "British Shorthair";
+
+const kittenThreeImage = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
+const kittenThreeName = "Cielo";
+const kittenThreeDesc = "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenThreeRace = "British Shorthair";
+
+// Guardar <li> en variables (añadidas interpolaciones)
+
+/* ¿Añadir estilos para poner en línea la lista? */
+
+const catList = document.querySelector(".js-list");
+
+const kittenOne = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src=${kittenOneImage}
+    alt="gatito"
+  />
+  <h3 class="card_title">${kittenOneName}</h3>
+  <h4 class="card_race">${kittenOneRace}</h4>
+  <p class="card_description">${kittenOneDesc}</p>
+</article>
+</li>`;
+
+const kittenTwo = `<li class="card">
+<img
+  class="card_img"
+  src=${kittenTwoImage}
+  alt="gatito"
+/>
+<h3 class="card_title">${kittenTwoName}</h3>
+<h4 class="card_race">${kittenTwoRace}</h4>
+<p class="card_description">${kittenTwoDesc}</p>
+</li>`;
+
+const kittenThree = `<li class="card">
+<img
+  class="card_img"
+  src=${kittenThreeImage}
+  alt="gatito"
+/>
+<h3 class="card_title">${kittenThreeName}</h3>
+<h4 class="card_race">${kittenThreeRace}</h4>
+<p class="card_description">${kittenThreeDesc}</p>
+</li>`;
+
+catList.innerHTML = kittenOne + kittenTwo + kittenThree; 
+
+
+
+
